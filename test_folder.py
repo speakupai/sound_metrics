f9b299aef3b8e3366fc92f87344ab961d13031a1
Rana Ahmad <taimur1871@gmail.com>
2021-01-22 15:48:49 -0700

    add new script for folder

---
+++ test_folder.py
@@ -0,0 +1,32 @@
+import numpy as np
+import librosa
+from metrics import pesq_score, stoi_score
+import argparse
+from downsample import downsample
+
+# create arguments
+parser = argparse.ArgumentParser()
+ 
+parser.add_argument('--original', required=True,
+    help='path to original file')
+  
+parser.add_argument('--clean', required=True,
+    help='path to processed file')
+
+
+# get arguments back
+get_inputs = vars(parser.parse_args())
+file_original=get_inputs['original']
+file_clean=get_inputs['clean']
+
+# load file
+snd_orig, sr_0 = librosa.load(file_original)
+snd_denoise, sr_1 = librosa.load(file_clean)
+
+if sr_0 != 16000 or sr_0 != 8000:
+    snd_orig = downsample(snd_orig)
+    snd_denoise = downsample(snd_denoise)
+    sr_0 = 16000
+
+print('PESQ: ', pesq_score(snd_orig, snd_denoise, samplerate=16000))
+#print('STOI: ', stoi_score(snd_orig, snd_denoise, samplerate=16000))
