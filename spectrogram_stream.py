4240319d5cf9ce7bfc445591ab7cdd70bc04dd3f
Rana Ahmad <taimur1871@gmail.com>
2021-02-05 13:41:51 -0700

    subsampled spectrogram

--- spectrogram_stream.py
+++ spectrogram_stream.py
@@ -19,22 +19,26 @@ file_list = os.listdir(audio_folder)
 os.chdir(audio_folder)
 
 
-for fname in file_list:
-   # load audio
-   start_time = time.time()
-   snd, sr = librosa.load(fname, sr=16000)
-   print(time.time()-start_time)
-
-   spect = melspectrogram(y=snd, sr=sr)
-   fig, ax = plt.subplots()
-   S_dB = librosa.power_to_db(spect, ref=np.max)
-   img = librosa.display.specshow(S_dB, x_axis='time',
-                                  y_axis='mel', sr=sr,
-                                  fmax=8000, ax=ax)
-   fig.colorbar(img, ax=ax, format='%+2.0f dB')
-   ax.set(title='Mel-frequency spectrogram')
-
-   print(fname[:-4])
-   fig.savefig(os.path.join(save_folder, fname[:-4]+'.png'), format='png')
+for fname in file_list[:1]:
+   # find duration
+   clip_len = librosa.get_duration(filename = fname, sr=16000)
    
-   plt.show()
+   # load audio
+   for sub_clip in range(0, int(clip_len), 10):
+      start_time = time.time()
+      snd, sr = librosa.load(fname, sr=16000, offset=sub_clip, duration=10)
+      print(time.time()-start_time)
+      print(snd.shape)
+      spect = melspectrogram(y=snd, sr=sr)
+      fig, ax = plt.subplots()
+      S_dB = librosa.power_to_db(spect, ref=np.max)
+      img = librosa.display.specshow(S_dB, x_axis='time',
+                                     y_axis='mel', sr=sr,
+                                     fmax=8000, ax=ax)
+      fig.colorbar(img, ax=ax, format='%+2.0f dB')
+      ax.set(title='Mel-frequency spectrogram')
+
+      print(fname[:-4])
+      fig.savefig(os.path.join(save_folder, str(sub_clip)+'.png'), format='png')
+      
+      plt.show()
