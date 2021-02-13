f34ef709ce677989e841c8a704e82537eeceedc8
Rana Ahmad <taimur1871@gmail.com>
2021-02-12 21:56:35 -0700

    update spect test

--- spectrogram_test.py
+++ spectrogram_test.py
@@ -8,25 +8,19 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-audio_file=input('enter original file path:\n')
+#audio_file=input('enter original file path:\n')
+audio_file = 'sample_audio/script5/f1_script5_ipad_office1_denoised.wav'
 
 # load audio
-def load_audio(audio_file):
-   snd, sr = librosa.load(audio_file, sr=16000) 
+snd, sr = librosa.load(audio_file, sr=16000) 
 
-   return snd, sr
-
-def create_spectrogram(snd_file, srate):
-   spect = melspectrogram(y=snd_file, sr=srate)
-   fig, ax = plt.subplots()
-   S_dB = librosa.power_to_db(spect, ref=np.max)
-   img = librosa.display.specshow(S_dB, x_axis='time',
-                         y_axis='mel', sr=srate,
-                         fmax=8000, ax=ax)
-   fig.colorbar(img, ax=ax, format='%+2.0f dB')
-   ax.set(title='Mel-frequency spectrogram')
+spect = melspectrogram(y=snd, sr=sr)
+fig, ax = plt.subplots()
+S_dB = librosa.power_to_db(spect, ref=np.max)
+img = librosa.display.specshow(S_dB, x_axis='time',
+                        y_axis='mel', sr=sr,
+                        fmax=8000, ax=ax)
+fig.colorbar(img, ax=ax, format='%+2.0f dB')
+ax.set(title='Mel-frequency spectrogram')
    
-   plt.show()
-
-sound_clip, sample_rate = load_audio(audio_file)
-create_spectrogram(sound_clip, sample_rate)
+plt.show()
