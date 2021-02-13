414f71dcd252ba29818c3bdee282f819269f9082
Rana Ahmad <taimur1871@gmail.com>
2021-02-13 10:57:36 -0700

    add new spec file

--- spectrogram_test.py
+++ spectrogram_test.py
@@ -19,7 +19,7 @@ fig, ax = plt.subplots()
 S_dB = librosa.power_to_db(spect, ref=np.max)
 img = librosa.display.specshow(S_dB, x_axis='time',
                         y_axis='mel', sr=sr,
-                        fmax=8000, ax=ax)
+                        fmax=16000, ax=ax)
 fig.colorbar(img, ax=ax, format='%+2.0f dB')
 ax.set(title='Mel-frequency spectrogram')
    
